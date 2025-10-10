0c5708fe315f312768877db46a828c6155513e2c
rafaelromanini <rafaromaninideoliveira@gmail.com>
2025-10-09 21:00:11 -0300

    feat(doctor): create request and response DTOs with validation patterns

--- src/main/java/com/hospitalcare/dto/DoctorRequestDTO.java
+++ src/main/java/com/hospitalcare/dto/DoctorRequestDTO.java
@@ -1,4 +1,36 @@
 package com.hospitalcare.dto;
 
-public record DoctorRequestDTO() {
-}
+import com.hospitalcare.model.enums.Specialty;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+
+public record DoctorRequestDTO(
+   @NotBlank(message = "Name is required.")
+   @Pattern(regexp = "^[A-Za-zÀ-ÿ\\s]+$", message = "Name must contain only letters.")
+   String name,
+
+   @NotBlank(message = "CPF is required.")
+   @Pattern(
+           regexp = "^\\d{3}\\.?\\d{3}\\.?\\d{3}-?\\d{2}$",
+           message = "CPF must be in the format 000.000.000-00 or only numbers."
+   )
+   String cpf,
+
+   @NotBlank(message = "CRM is required.")
+   @Pattern(
+           regexp = "^[0-9]{9}-[0-9]/[A-Z]{2}$",
+           message = "CRM must be in the format 000000000-0/UF"
+   )
+   String crm,
+
+   @NotNull(message = "Specialty is required.")
+   Specialty specialty,
+
+   @Pattern(
+           regexp = "^\\(?\\d{2}\\)?\\s?9?\\d{4}-?\\d{4}$",
+           message = "Phone number must be valid, e.g. (11)99999-8888."
+   )
+   String phone
+
+) {}
--- src/main/java/com/hospitalcare/dto/responses/DoctorResponseDTO.java
+++ src/main/java/com/hospitalcare/dto/responses/DoctorResponseDTO.java
@@ -1,4 +1,12 @@
 package com.hospitalcare.dto.responses;
 
-public record DoctorResponseDTO() {
-}
+import com.hospitalcare.model.enums.Specialty;
+
+public record DoctorResponseDTO(
+        Long id,
+        String name,
+        String cpf,
+        String crm,
+        Specialty specialty,
+        String phone
+) {}
